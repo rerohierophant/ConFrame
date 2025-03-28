20b5dd091706a9a6b3e8838f6b4b189d9bf0b799
rerohierophant <3044769525@qq.com>
2025-03-29 01:04:51 +0800

    index

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,22 @@ module.exports = {
         '2xl': '1536px',
         '3xl': '1920px'
       },
+      fontFamily:{
+        'audiowide': ['Audiowide', 'sans-serif'],
+        'pingfang': ['PingFang SC', 'system-ui', 'sans-serif']
+      },
+      fontSize: {
+        '14':'0.875rem',
+        '16':'1rem',
+        '18':'1.125rem',
+        '20':'1.25rem',
+        '22':'1.375rem',
+        '24':'1.5rem',
+        '32':'2rem',
+        
+      },
+
+
     },
   },
   plugins: [],
